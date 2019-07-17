874916b2d85933e036f3ebb9bebfcc4f0ac12c8a
Jerry Nyoike <jngugi31@gmail.com>
2019-07-17 18:44:44 +0300

    Add logic for image uploads

--- askfortransport.py
+++ askfortransport.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify, request, make_response
+from werkzeug.utils import secure_filename
 import pymysql.cursors
 from datetime import datetime, timedelta
-from jwt import encode, decode, get_unverified_header
-import crypt
+import jwt
+import os
 
 # create a secret for signing jwt's, this should later on be set in
 # the environment variables for security purposes
-secret = crypt.crypt('secreto', salt=crypt.METHOD_SHA256)
+secret = "$5$3Y/MPbv8Qi2nDv5M$z9rn56J9EDnw.bOtCpJSelh76uzkUsUHaDnHZ73D9g."
+ALLOWED_EXTENSIONS = ['jpeg', 'png', 'jpg']
 
-# connect to mysql db
+# connect to mysql dbm
 connection = pymysql.connect(host='localhost',
                              user='ulembaya',
                              password='Mby4d3$$t',
@@ -18,6 +20,27 @@ connection = pymysql.connect(host='localhost',
 # obtain cursor to enable us to make db queries
 cur = connection.cursor()
 
+app = Flask(__name__)
+app.config.from_mapping(
+        IMAGE_STORE_PATH='/home/nyoike/Documents/images/'
+)
+
+
+def fetch_user(username, password, user_type):
+        user = None
+        with connection.cursor() as cur:
+                if user_type is 'client':
+                        cur.execute("SELECT * FROM user WHERE username = '{}' AND pwd = '{}' LIMIT 1".format(username, password))
+                        connection.commit()
+                        user = cur.fetchone()
+
+                elif user_type is 'transporter':
+                        cur.execute("SELECT * FROM transporter WHERE username = '{}' AND pwd = '{}' LIMIT 1".format(username, password))
+                        connection.commit()
+                        user = cur.fetchone()
+
+        return user
+
 @app.route('/auth/register/<user_type>', methods=['POST'])
 def do_registration(user_type):
         '''Get the users's information from POST data and inserts into the database'''
@@ -25,6 +48,27 @@ def do_registration(user_type):
         result, status = register_user(request_data, user_type)
         return make_response(jsonify(result), status)
                 
+
+def allowed_file(filename):
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def decoded_token(tkn):
+        if verify_token(tkn):
+                return jwt.decode(tkn)
+        else:
+                return None
+
+
+def verify_token(token):
+        ''' whenever a user sends a request accompanied by the jwt
+            this function checks its validity by looking at the algorithm used and the expiry time of the token'''
+        try:
+                jwt.decode(token, secret, algorithms=['HS256'])
+                return True
+        except jwt.ExpiredSignatureError:
+                return False
+        except jwt.InvalidAlgorithmError:
+                return False
 
 @app.route('/auth/login/client', methods=['POST'])
 def do_client_login():
@@ -35,10 +79,10 @@ def do_client_login():
         if user_info is None:
                 return jsonify({'message': 'User not found'})
 
-        elif user_info['password'] == request_data['pass'] and user_info['username'] == request_data['username']:
-                jwt = encode({'sub': user_info['id'], 'exp': datetime.now()+timedelta(
-                        days=10)}, secret, algorithm='HS256')
-                return jsonify({'message': 'Successful login', 'token': '{}'.format(jwt)})
+        elif user_info['pwd'] == request_data['pass'] and user_info['username'] == request_data['username']:
+                token = jwt.encode({'sub': user_info['id'], 'exp': datetime.utcnow()+timedelta(
+                        days=10)}, secret, algorithm='HS256').decode('utf-8')
+                return jsonify({'message': 'Successful login', 'token': '{}'.format(token)})
 
         else:
                 response = make_response(jsonify({'message': 'User not found'}), 404)
@@ -52,10 +96,10 @@ def do_driver_login():
         if user_info is None:
                 return jsonify({'message': 'User not found'})
 
-        elif user_info['password'] == request_data['pass'] and user_info['username'] == request_data['username']:
-                jwt = encode({'sub': user_info['id'], 'exp': datetime.now()+timedelta(
-                        days=10)}, secret, algorithm='HS256')
-                return jsonify({'message': 'Successful login', 'token': '{}'.format(jwt)})
+        elif user_info['pwd'] == request_data['pass'] and user_info['username'] == request_data['username']:
+                token = jwt.encode({'sub': user_info['id'], 'exp': datetime.now()+timedelta(
+                        days=10)}, secret, algorithm='HS256').decode('utf-8')
+                return jsonify({'message': 'Successful login', 'token': '{}'.format(token)})
 
         else:
                 response = make_response(jsonify({'message': 'User not found'}), 404)
@@ -118,14 +162,34 @@ def fetch_user(username, password, user_type):
 
         return cur.fetchone()
 
+@app.route('/images/vehicle/upload', methods=['POST'])
+def upload_image():
+        #get token and check token validity
+        token = request.headers['Authorization'].split(" ")[1]
+        #if verify_token(token) and request.headers['Content-Type'] == 'multipart/form-data':
+        if not request.files:
+                response = make_response(jsonify({"message": "no files uploaded"}), 400)
+                return response
+        
+        img = request.files['file']
+        if img.filename == '':
+                response = make_response(jsonify({"message": "no uploaded"}), 400)
+                return response
 
-def verify_token(token):
-        ''' whenever a user sends a request accompanied by the jwt 
-            we must check its validity'''
-        payload = decode(token, secret, algorithm='HS256')
-        if get_unverified_header(token)['alg'] is 'HS256':
-                return True
-        elif datetime.now().month - payload['exp'].month >= 1:
-                return False
-        else:
-                return True
+        elif img and allowed_file(img.filename):
+                driver_id = jwt.decode(token, secret, algorithms=['HS256'])['sub']
+                filename = secure_filename(img.filename)
+                path = os.path.join(app.config['IMAGE_STORE_PATH'], str(driver_id), filename)
+                if not os.path.exists(os.path.dirname(path)):
+                        os.makedirs(os.path.dirname(path))
+
+                img.save(path)
+                # save the file path to the database
+                with connection.cursor() as cur:
+                        cur.execute("UPDATE vehicle SET pictures = %s WHERE transporter_id = %s", (path, driver_id))
+                        connection.commit()
+                
+                return jsonify({"message":"Successfully uploaded image"})
+                
+        response = make_response(jsonify({"message":"Bad request"}, 400))
+        return response
