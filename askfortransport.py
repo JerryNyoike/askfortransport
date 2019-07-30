9fd00f62eecfd3d3e831f8c41b5dd7add6b917ad
Brackie <muimikieleko@gmail.com>
2019-07-30 23:49:14 +0300

    Added endpoint for vehicle registration

--- askfortransport.py
+++ askfortransport.py
@@ -46,7 +46,7 @@ def do_registration(user_type):
         '''Get the users's information from POST data and inserts into the database'''
         request_data = request.get_json(force=True)
         result, status = register_user(request_data, user_type)
-        return make_response(jsonify(result), status)
+        return make_cross_response(jsonify(result), status)
                 
 
 def allowed_file(filename):
@@ -85,25 +85,23 @@ def do_client_login():
                 return jsonify({'message': 'Successful login', 'token': '{}'.format(token)})
 
         else:
-                response = make_response(jsonify({'message': 'User not found'}), 404)
-                return response
+                return make_cross_response(jsonify({'message': 'User not found'}), 404)
 
 @app.route('/auth/login/driver', methods=['POST'])
 def do_driver_login():
         request_data = request.get_json(force=True)
         user_info = fetch_user(request_data['username'], request_data['pass'], 'transporter')
 
-        if user_info is None:
-                return jsonify({'message': 'User not found'})
-
+        if not user_info:
+                return make_cross_response(jsonify({'message': 'User not found'}), 404)
         elif user_info['pwd'] == request_data['pass'] and user_info['username'] == request_data['username']:
-                token = jwt.encode({'sub': user_info['id'], 'exp': datetime.now()+timedelta(
-                        days=10)}, secret, algorithm='HS256').decode('utf-8')
-                return jsonify({'message': 'Successful login', 'token': '{}'.format(token)})
-
+                jwt = encode({'sub': user_info['id'], 'exp': datetime.now()+timedelta(
+                        days=10)}, secret, algorithm='HS256')
+                del user_info["pwd"]
+                response = jsonify({'message': 'Successful login', 'token': '{}'.format(jwt), 'details':'{}'.format(user_info)})
+                return make_cross_response(response, 200)
         else:
-                response = make_response(jsonify({'message': 'User not found'}), 404)
-                return response
+                return make_cross_response(jsonify({'message': 'User not found'}), 404)
 
 @app.route("/vehicles", methods=['POST'])
 @app.route("/vehicles/<search_params>", methods=['POST'])
@@ -123,9 +121,28 @@ def get_vehicles(search_params=None):
         cur.execute(fetch_query) 
         result = cur.fetchall()
         if not result:
-                return make_response(jsonify({'success': 0, 'message': 'No vehicles found'}), 404)
+                return make_cross_response(jsonify({'success': 0, 'message': 'No vehicles found'}), 404)
         else:
-                return make_response(jsonify({'success': 1, 'vehicles': result}), 200)
+                return make_cross_response(jsonify({'success': 1, 'vehicles': result}), 200)
+
+@app.route("/register_vehicle/", methods=['POST'])
+def register_vehicle():
+        body = request.get_json(force=True)
+
+        token_payload = decode_token(body["token"])
+        if not token_payload:
+                return make_cross_response(jsonify({"success": 0, "message": "Driver don't exist"}), 404)
+
+        insert_query = "INSERT INTO vehicle (type, capacity, price, number_plate, pictures, transporter_id) VALUES "
+        insert_query += "('{}', '{}', '{}', '{}', 'no image', {}, 'available')".format(body["type"], body["capacity"], body["price"], body["number_plate"], token_payload["sub"])
+        cur.execute(insert_query)
+        connection.commit()
+        if cur.rowcount > 0:     
+                fetch_query = "SELECT id FROM vehicle WHERE number_plate = '%s'" % body["number_plate"]
+                cur.execute(fetch_query)
+                response = jsonify({"success": 1, "vehicle_id": cur.fetchone()["id"]})
+                return make_cross_response(response, 200)
+
 
 def register_user(user_details, user_type):                
         #check if the user already exists
@@ -141,13 +158,16 @@ def register_user(user_details, user_type):
                         insert_query += "VALUES ('{}', '{}', {}, '{}')".format(user_details["email"], 
                         user_details["username"], user_details["phone"], user_details["pwd"])
                 elif user_type == 'transporter':
-                        insert_query += "INSERT INTO transporter (email, username, dl_number, fullname, phone, pwd) "
+                        insert_query += "INSERT INTO transporter (email, username, dl_number, full_name, phone, pwd) "
                         insert_query += "VALUES ('{}', '{}', '{}', '{}', {}, '{}')".format(user_details["email"], 
-                        user_details["username"], user_details["dl_number"], user_details["fullname"], 
+                        user_details["username"], user_details["dl_number"], user_details["full_name"], 
                         user_details["phone"], user_details["pwd"])
                 cur.execute(insert_query) 
                 connection.commit()
-                return {'success': 1, 'message': 'Registration succesful'}, 200
+                if cur.rowcount > 0:
+                        return {'success': 1, 'message': 'Registration succesful'}, 200
+                else:
+                        return {'success': 0, 'message': 'Server error while inserting data'}, 500
         elif user_details["email"] == result["email"]:
                 return {'success': 0, 'message': 'Email already exists'}, 409
         elif user_details["username"] == result["username"]:
@@ -189,3 +209,16 @@ def upload_image(vehicle_id):
                         
                 response = make_response(jsonify({"message":"Bad request"}, 400))
                 return response
+        
+def fetch_user(username, password, user_type):
+        if user_type == 'client':
+                cur.execute("SELECT * FROM user WHERE username = '{}' AND pwd = '{}' LIMIT 1".format(username, password))
+        elif user_type == 'transporter':
+                cur.execute("SELECT * FROM transporter WHERE username = '{}' AND pwd = '{}' LIMIT 1".format(username, password))    
+        return cur.fetchone()
+
+
+def make_cross_response(data, code):
+        response = make_response(data, code)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
