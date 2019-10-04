3416982a927394be043af508fac22f4630c9fef2
JerryNyoike <jngugi31@gmail.com>
2019-10-04 12:00:37 +0300

    Add module for wokring with the database

--- aft/__init__.py
+++ aft/__init__.py
@@ -1,22 +1,29 @@
 import os
+import pymysql.cursors
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
+from . import db
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='$5$3Y/MPbv8Qi2nDv5M$z9rn56J9EDnw.bOtCpJSelh76uzkUsUHaDnHZ73D9g.',
         IMAGE_STORE_PATH='static',
-        DATABASE=connection
+        DB_HOST='localhost',
+        DB_USER='root',
+        DB_PASS='rootlikesmysql-db',
+        DB='askfortransport',
+        CURSOR=pymysql.cursors.DictCursor
     )
     CORS(app)
     if test_config is None:
-        app.config.from_mapping('config.py', silent=True)
+        pass
+        # app.config.from_mapping('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
 
     try:
-        os.make_dirs(app.instance_path)
+        os.makedirs(app.instance_path)
     except (OSError):
         pass
 
---
+++ aft/db.py
@@ -0,0 +1,43 @@
+import pymysql.cursors
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = pymysql.connect(host=current_app.config['DB_HOST'],
+                             user=current_app.config['DB_USER'],
+                             password=current_app.config['DB_PASS'],
+                             db=current_app.config['DB'],
+                             cursorclass=current_app.config['CURSOR'])
+    
+    return g.db.cursor()
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+    
+    with current_app.open_resource('./schema.sql') as f:
+        queries = f.read().decode('utf-8').replace('\n', ' ').replace('/t', ' ').split(';')
+        for num, query in enumerate(queries[:len(queries)-1]):
+            db.execute(query)
+            # print(num, query, '\n')
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
