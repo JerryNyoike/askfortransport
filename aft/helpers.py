f48d7debca2d5f380ea9cfbf31c1b273df223ece
JerryNyoike <jngugi31@gmail.com>
2019-10-07 22:35:10 +0300

    Add required imports

--- aft/helpers.py
+++ aft/helpers.py
@@ -1,3 +1,6 @@
+import jwt
+
+
 def verify_token(token, secret):
         ''' whenever a user sends a request accompanied by the jwt
             this function checks its validity by looking at the algorithm used and the expiry time of the token'''
