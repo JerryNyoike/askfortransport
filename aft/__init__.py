8196063d22e02a3641c6de375858d6ea2f7999c9
JerryNyoike <jngugi31@gmail.com>
2019-10-15 16:16:30 +0300

    Read in daraja API keys from the config.py file

--- aft/__init__.py
+++ aft/__init__.py
@@ -18,8 +18,7 @@ def create_app(test_config=None):
     )
     CORS(app)
     if test_config is None:
-        pass
-        # app.config.from_mapping('config.py', silent=True)
+        app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
 
